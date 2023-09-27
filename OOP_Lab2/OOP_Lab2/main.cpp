aaf003d1ec87aa95d98ea425842cf730ba143bec
GittyupTest <mailforgit@rambler.ru>
2023-09-27 12:09:52 +0700

    Реализована функция вычисления среднего значения Массива

--- OOP_Lab2/OOP_Lab2/main.cpp
+++ OOP_Lab2/OOP_Lab2/main.cpp
@@ -2,12 +2,24 @@
 
 #include "Array.h"
 
+double calculateAvg(Array arr)
+{
+	double avg = 0;
+	for (int i = 0; i < arr.size(); ++i) {
+		avg += arr[i];
+	}
+	avg /= arr.size();
+	return avg;
+}
+
 int main()
 {
 	Array arr(10, 0);
+	int a = arr[3];
 	std::cout << arr[3] << ' ' << arr[9] << std::endl;
-	arr[3] = 4;
+	arr[3] = 16;
 	arr[5] = arr[3];
+	std::cout << "Average value: " << calculateAvg(arr) << '\n';
 	arr.print();
 
 	getchar();
