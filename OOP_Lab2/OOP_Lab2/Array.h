61497e96872c03c01fb9138902846634e1742cec
GittyupTest <mailforgit@rambler.ru>
2023-09-27 11:16:01 +0700

    Создан проект класса Массив
    Реализованы конструктор по умолчанию с параметрами, деструктор, print, operator[].

---
+++ OOP_Lab2/OOP_Lab2/Array.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class Array
+{
+public:
+	Array(const int size = 10, const int value = 0);
+	~Array();
+
+	void print() const;
+
+	int operator[](const int index);
+
+private:
+	int *m_array = nullptr;
+	int m_size = 0;
+};
+
---
+++ OOP_Lab2/OOP_Lab2/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "Array.h"
+
+int main()
+{
+	Array arr(10, 0);
+	std::cout << arr[3] << ' ' << arr[9] << std::endl;
+	//arr[3] = 4;
+	//arr[5] = arr[3];
+	arr.print();
+
+	getchar();
+	return 0;
+}
